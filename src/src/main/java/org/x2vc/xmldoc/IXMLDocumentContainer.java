59863cbc03bf0650f6977e51eaeb86355bdcdbac
Volker Wegert <mail@volker-wegert.de>
2023-08-01 18:03:53 +0200

    added initial (incomplete) XML document container interface

---
+++ src/src/main/java/org/x2vc/xmldoc/IXMLDocumentContainer.java
@@ -0,0 +1,35 @@
+package org.x2vc.xmldoc;
+
+import org.x2vc.schema.IXMLSchema;
+import org.x2vc.stylesheet.IStylesheetInformation;
+
+/**
+ * A container object that is used to transport a generated XML document, its
+ * corresponding descriptor and a reference to the request used to generate the
+ * document. The container also references the XML schema version used to
+ * generate the document.
+ */
+public interface IXMLDocumentContainer {
+
+	/**
+	 * @return the {@link IXMLSchema} used to generate this document
+	 */
+	IXMLSchema getSchema();
+
+	/**
+	 * Shortcut for <code>getSchema().getStylesheet()</code>
+	 *
+	 * @return the stylesheet for which this input document was generated
+	 */
+	IStylesheetInformation getStylesheet();
+
+	/**
+	 * @return the generated XML document
+	 */
+	String getDocument();
+
+	// TODO XML Document: add missing accessors: descriptor
+	// TODO XML Document: add missing accessors: request
+	// TODO XML Document: add missing accessors: schema and version
+
+}
