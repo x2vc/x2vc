a08ece3a4b4509437bd21192d78ccc5955fb0035
Volker Wegert <github@volker-wegert.de>
2023-11-14 15:08:51 +0100

    fixed typos

--- src/main/java/org/x2vc/xml/document/IModifierPayload.java
+++ src/main/java/org/x2vc/xml/document/IModifierPayload.java
@@ -9,13 +9,13 @@ package org.x2vc.xml.document;
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  * #L%
  */
 
 /**
- * Interface for additional payload that may be carried by a
+ * Interface for additional payload that may be carried by an
  * {@link IDocumentModifier}.
  */
 public interface IModifierPayload {
