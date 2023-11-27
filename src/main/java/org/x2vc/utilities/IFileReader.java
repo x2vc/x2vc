37d3c86c53e84baa376962aea79bb9e7e5044daa
Volker Wegert <mail@volker-wegert.de>
2023-11-27 22:53:03 +0100

    added license headers

--- src/main/java/org/x2vc/utilities/IFileReader.java
+++ src/main/java/org/x2vc/utilities/IFileReader.java
@@ -1,3 +1,16 @@
+/*-
+ * #%L
+ * x2vc - XSLT XSS Vulnerability Checker
+ * %%
+ * Copyright (C) 2023 x2vc authors and contributors
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
 package org.x2vc.utilities;
 
 import java.io.File;
